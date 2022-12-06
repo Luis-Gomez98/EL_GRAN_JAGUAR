bccc119c2a54ab1d986655334f536ff692a598dd
Luis-Gomez98 <gom18291@uvg.edu.gt>
2022-12-05 23:11:09 -0600

    Limpiando el código
    Se realiza una limpia de código. Eliminando variables que no se utilizan y realizando más comentarios.

--- main.c
+++ main.c
@@ -7,6 +7,8 @@
  * Luis Ricardo Gómez Velásquez - 18291
  */
 
+//Liberías Utilizadas
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,9 +26,7 @@
 #include "driverlib/uart.h"
 #include "utils/uartstdio.h"
 
-bool BUSDATOS[8], ring_o;
-int x = 0;
-char str;
+
 //****************************** Creación de las funciones delay ***********************************************
 
 void delayMs(uint32_t ui32Ms) {
@@ -71,10 +71,15 @@ InitConsole(void)
     // Initialize the UART for console I/O.
     UARTStdioConfig(0, 115200, 16000000);
 }
-//************************************************************************************************************
+//************************************************** Main **********************************************************
 
 int main(void)
 {
+    //Declaración Variables
+    bool BUSDATOS[8];
+    int x = 0;
+    char str;
+
     SysCtlClockSet(SYSCTL_SYSDIV_2_5 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
                    SYSCTL_XTAL_16MHZ); // 80 MHz
 
@@ -101,14 +106,14 @@ int main(void)
     while(1)
     {
         while(UARTCharsAvail(UART0_BASE)){
-                    str = UARTCharGet(UART0_BASE);
+                    str = UARTCharGet(UART0_BASE);  //str recibe los datos mediante el UART0
                     }
 
-        if ((str == 'S')){
-            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, 255); //EN
+        if ((str == 'S')){ //Inicia la automatización
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, 255); //EN en 1 cuando se reciba el carácter 'S'
 
-            if (x == 0){
-                UARTprintf("00001010\n");
+            if (x == 0){ //Si el contador es cero
+                UARTprintf("00001010\n"); //Saltos de línea
                 UARTprintf("00001010\n");
                 GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, 255); //Reset en 1
                 delayUs(1);
@@ -116,7 +121,7 @@ int main(void)
                 GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, 0); //Select en 00
                 GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, 0);
             }else if (x == 3537){ //Cuando termine de desplegar el primer texto, se activa un reset y el select cambia a 1
-                UARTprintf("00001010\n");
+                UARTprintf("00001010\n"); //Saltos de línea
                 UARTprintf("00001010\n");
                 GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, 255); //Reset en 1
                 delayUs(1);
@@ -124,18 +129,18 @@ int main(void)
                 GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, 0); //Select en 01
                 GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, 255);
             }else if (x == 6930){ //Cuando termine de desplegar ambos textos, se activa un reset y el select cambia a 0
-                UARTprintf("00001010\n");
+                UARTprintf("00001010\n"); //Saltos de línea
                 UARTprintf("00001010\n");
                 GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, 255); //Reset en 1
                 delayUs(1);
                 GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, 0); //Reset en 0
                 GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, 0); //Select en 00
                 GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, 0);
-                x = 0;
+                x = 0; //Se reinicia el contador para volver a desplegar el primer texto
             }
 
 
-            //Leer datos
+            //Lectura de datos transmitidos por el FPGA
             BUSDATOS[0] = GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_7);
             BUSDATOS[1] = GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_6);
             BUSDATOS[2] = GPIOPinRead(GPIO_PORTA_BASE, GPIO_PIN_5);
@@ -144,27 +149,26 @@ int main(void)
             BUSDATOS[5] = GPIOPinRead(GPIO_PORTE_BASE, GPIO_PIN_4);
             BUSDATOS[6] = GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_1);
             BUSDATOS[7] = GPIOPinRead(GPIO_PORTB_BASE, GPIO_PIN_0);
-            ring_o = GPIOPinRead(GPIO_PORTF_BASE, GPIO_PIN_1);
 
             UARTprintf("%i%i%i%i%i%i%i%i\n", BUSDATOS[7],BUSDATOS[6],BUSDATOS[5],BUSDATOS[4],BUSDATOS[3],BUSDATOS[2],BUSDATOS[1],BUSDATOS[0]);
+            //Envío de datos por UART
 
-
-            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, 0); //CLK que entra al FPGA
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, 0); //CLK que entra al FPGA cada 7 ms
             delayMs(7);
             GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, 255);
             delayMs(7);
 
-            x++;
+            x++; //Incrementa el contador
         }else if ((str == 'P')){
             GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, 0);
-            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, 0); //EN
+            GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, 0); //EN en 0 cuando se reciba el carácter 'P'
         }else{
             GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_2, 0); //CLK que entra al FPGA
             GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, 0); //Reset en 0
             GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, 0); //Select en 00
             GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, 0);
             GPIOPinWrite(GPIO_PORTD_BASE, GPIO_PIN_3, 0); //EN
-            x = 0;
+            x = 0; //Inicia en 0 el contador
         }
     }
 }
